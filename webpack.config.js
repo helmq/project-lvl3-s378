d89e7527c35c275aff436e13cf1efe10973841ba
Viktor <jarlaks52@gmail.com>
2019-01-07 21:56:22 +0900

    init webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'RSS reader',
+      template: 'template.html',
+    }),
+  ],
+  devServer: {
+    contentBase: './dist',
+  },
+};
