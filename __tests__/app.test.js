84e24c46b66d93d196fbb96aadce96454e750d75
Viktor <jarlaks52@gmail.com>
2019-01-11 22:45:51 +0900

    update tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,13 +1,24 @@
 import { promises as fs } from 'fs';
 import path from 'path';
+import { html } from 'js-beautify';
+import timer from 'timer-promise';
 import init from '../src/init';
 
 beforeEach(async () => {
   const pathToHtml = path.resolve(__dirname, '__fixtures__/index.html');
-  const html = await fs.readFile(pathToHtml, 'utf8');
-  document.body.innerHTML = html;
+  const innerHTML = await fs.readFile(pathToHtml, 'utf8');
+  document.body.innerHTML = innerHTML;
 });
 
-test('test app', () => {
+const getTree = () => html(document.body.innerHTML);
+
+test('test incorrect url', async () => {
   init();
+  const form = document.getElementById('rss-form');
+  const input = form.elements.url;
+  const button = form.elements.submit;
+  input.value = 'test';
+  button.click();
+  await timer.start('test', 100);
+  expect(getTree()).toMatchSnapshot();
 });
